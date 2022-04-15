00503bbc9b61055bb7b14fc7ab7fbdab9d63cfc5
tasnimul2 <chowdhury1478@gmail.com>
2022-04-15 16:32:59 -0400

    added more comments to the code

--- DT_mohammed_chowdhury.py
+++ DT_mohammed_chowdhury.py
@@ -37,12 +37,12 @@ class DecisionTreeModel:
     
     def fit(self, X: pd.DataFrame, y: pd.Series):
         '''Implement the code here, this is a overloaded method that will handle if a dataframe or a pd series 
-        is passed in. It converts it into a numpy array so the orginal _fit method functions properly.'''
+        is passed in. It converts it into a numpy array so the orginal _fit method functions properly.
+        Before converting  y into a numpy array, it checks to see if it is has categorical values. If it does, then 
+        change the values to 0's and 1's.
+        '''
         # call the _fit method
         new_X = X.to_numpy()
-        
-        
-        
         does_have_catigorical_values_in_list = [type(x) == int for x in y]
         if(not any(does_have_catigorical_values_in_list)):
             y = y.apply(lambda x: 0 if x == 'M' else 1)
@@ -80,6 +80,7 @@ class DecisionTreeModel:
         return np.array(predictions)    
         
     def _is_finished(self, depth):
+        ''' This method checks if the tree's depth did not exceed the maximum. This is to about infinite depth first traversal'''
         # TODO: for graduate students only, add another stopping criteria
         # modify the signature of the method if needed
         if (depth >= self.max_depth
@@ -96,6 +97,18 @@ class DecisionTreeModel:
         return result
                               
     def _build_tree(self, X, y, depth=0):
+        '''
+        X.shape gets the number of rows and columns in the dataframe, which represents samples and features respectively. 
+        n_class_labels checks the number of unique labels there are. In this case there are 2 labels, "M and B" or "0 and 1". 
+        Then there is a check to see if the tree has hit maximum depth using is_finished() method. 
+        We then return the node with the value of the most common label. (answers : is M or B the most common?)
+        If it is not finished building then:
+        rnd_feats is a list of column numbers in the dataframe that represent different features.
+        best_split method is then called that returns the feature and threshold value of the split with the highest gini impurity / entorpy score.
+        Then recursively, pre-order traversal is done to build the children nodes. 
+        Finaly return the root node.
+        
+        '''
         self.n_samples, self.n_features = X.shape
         self.n_class_labels = len(np.unique(y))
 
@@ -128,7 +141,6 @@ class DecisionTreeModel:
             y_as_arr = y
         else :
             y_as_arr = y.values #y is a pd series, use pd.values to convert it to an np array
-        
         
         num_yes = 0
         num_no = 0
@@ -168,6 +180,7 @@ class DecisionTreeModel:
         
         
     def _create_split(self, X, thresh):
+        ''' Creates a left chid and right child node index for buid_tree method to utilze'''
         left_idx = np.argwhere(X <= thresh).flatten()
         right_idx = np.argwhere(X > thresh).flatten()
         return left_idx, right_idx
@@ -256,6 +269,10 @@ class RandomForestModel(object):
         # end TODO
 
     def fit(self, X: pd.DataFrame, y: pd.Series):
+        '''
+        depending on what the value of n_estimators is, a for loop will run to fit "n_estimators" number of decision trees and 
+        store that tree in the d_trees = [] list. But, the trees are fitted with bootstrapped data (which means the original data was randomized)
+        '''
         for i in range(self.n_estimators):
             currTree = DecisionTreeModel(max_depth=10)
             X_train,y_train = self.bootstrap_data(X,y)
@@ -275,6 +292,18 @@ class RandomForestModel(object):
         until no more elements are left. 
         result :
         [[1,4,7],[2,5,8],[3,6,9]]
+        
+        Then we loop through each linner list (tree_prediction) from the swapped_predictions_of_all_d_trees list derived from swapaxis()
+        We utilize the counter object to count the values inside the tree_prediction list (ie. count how me 1's are in the list and how many 0's in the list)
+        then we use the most_common() method to see which value, 0 or 1, occured the most. This method returns a list of tuple with this information
+        denoted: [(value,number to times it occured in the list)]. Ex. [(1,3)] would mean the value 1 occured 3 times in the list.
+        we use the notation counter.most_common(1)[0][0] to denote : 
+        - most_common(1) means get the 1st most common value. it is was most_common(2) it would mean 2nd most common. Result [(1,3)]
+        - most_common(1)[0] means get the 0th index inside the list of tuples. Result (1,3)
+        - counter.most_common(1)[0][0] means get the 0th index in that tuble. Result : 1. 
+        
+        this resulting value is then appended to the predictions list , which is then subsequently returned after the loop finishes running through all inner lists
+    
         '''
         predictions_of_all_d_trees = np.array([currTreeInList.predict(X) for currTreeInList in self.d_trees])
         swapped_predictions_of_all_d_trees = np.swapaxes(predictions_of_all_d_trees,0,1)
@@ -287,10 +316,11 @@ class RandomForestModel(object):
         
         return predictions
         
-    '''
-    The sample() method selects "num_data_samples" rows randomly.  
-    '''
+    
     def bootstrap_data(self,X,y):
+        '''
+        The sample() method selects "num_data_samples" rows randomly.  
+        '''
         num_data_samples = X.shape[0] #tells you number of rows in data frame
         bootstrapped_X = X.sample(num_data_samples)
         bootstrapped_y = y.sample(num_data_samples)
@@ -386,7 +416,7 @@ def _test():
     rf.fit(X_train,y_train)
     rf_y_pred = rf.predict(X_test)
     acc_rf = accuracy_score(y_test,rf_y_pred)
-    print(f"CLASSIFICATION REPORT for Decision Tree: \n {classification_report(y_test,rf_y_pred)}")
+    print(f"CLASSIFICATION REPORT for Random Forrest: \n {classification_report(y_test,rf_y_pred)}")
     print("ACCURACY:", acc_rf)
 
 if __name__ == "__main__":
